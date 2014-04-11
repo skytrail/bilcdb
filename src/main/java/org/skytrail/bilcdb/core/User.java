73ade36f3f3eb07c6e08a28b32e6ac927ac8d222
John Herndon <johnlherndon@gmail.com>
2014-04-10 21:49:51 -0600

    Rename to org.skytrail.bilcdb

---
+++ src/main/java/org/skytrail/bilcdb/core/User.java
@@ -0,0 +1,13 @@
+package org.skytrail.bilcdb.core;
+
+public class User {
+    private final String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
